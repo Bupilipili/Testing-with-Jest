a2472f4fef8e4c14c62914fa915891680519a8d3
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-21 17:55:24 +0300

    Update stringLength.test.js

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,6 +1,6 @@
 const stringLength = require('./stringLength'); // Replace with the correct path if using modules
 
-test('stringLength function returns the correct character count', () => {
+test('stringLength returns the correct character count', () => {
   const inputString = 'Hello, World!';
   const result = stringLength(inputString);
   expect(result).toBe(inputString.length);
